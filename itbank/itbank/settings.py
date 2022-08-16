debea42a3fd938a8666696dfa723a52549c4a7d6
Dominguez <santidzm0@gmail.com>
2022-08-16 00:20:08 -0300

    pasword reset anda

--- itbank/itbank/settings.py
+++ itbank/itbank/settings.py
@@ -125,4 +125,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
-
+LOGOUT_REDIRECT_URL = "/"
